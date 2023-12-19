f5bcbd313cb342e64f0fdfd87107dac30306f84f
Rezmii <bartoszrezmer20@gmail.com>
2023-12-19 15:46:33 +0100

    adding button which allows to change grid size

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
-const gridContainer = document.querySelector(".grid-container");
-let gridItem;
-let gridRow;
-for (let j = 0; j < 16; j++) {
-  gridRow = document.createElement("div");
-  gridRow.classList.add("grid-row");
-  gridContainer.appendChild(gridRow);
-  for (let i = 0; i < 16; i++) {
-    console.log("gridRow");
-    gridItem = document.createElement("div");
-    gridItem.classList.add("grid-item");
-    gridRow.appendChild(gridItem);
+function renderGrid(num) {
+  const gridContainer = document.querySelector(".grid-container");
+  let gridItem;
+  let gridRow;
+  for (let j = 0; j < num; j++) {
+    gridRow = document.createElement("div");
+    gridRow.classList.add("grid-row");
+    gridContainer.appendChild(gridRow);
+    for (let i = 0; i < num; i++) {
+      console.log("gridRow");
+      gridItem = document.createElement("div");
+      gridItem.classList.add("grid-item");
+      gridRow.appendChild(gridItem);
+    }
   }
+
+  const gridItems = document.querySelectorAll(".grid-item");
+
+  gridItems.forEach((singleItem) => {
+    singleItem.addEventListener("mouseover", () => {
+      singleItem.setAttribute("style", "background-color: lightcoral");
+    });
+  });
 }
 
-const gridItems = document.querySelectorAll(".grid-item");
+renderGrid(16);
 
-gridItems.forEach((singleItem) => {
-  singleItem.addEventListener("mouseover", () => {
-    singleItem.setAttribute("style", "background-color: lightcoral");
+const sizeButton = document.querySelector(".size-button");
+sizeButton.addEventListener("click", chooseSize);
+
+function chooseSize() {
+  let userChoice = prompt("Choose size: ");
+  const gridRows = document.querySelectorAll(".grid-row");
+  gridRows.forEach((singleItem) => {
+    singleItem.remove();
   });
-  singleItem.addEventListener("mouseout", () => {
-    singleItem.setAttribute("style", "background-color: lightblue");
-  });
-});
+  renderGrid(userChoice);
+}
